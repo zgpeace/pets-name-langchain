e5db07a21e6e14366c8e726ea0b1e5c85e166af8
zgpeace <zgpeace@gmail.com>
2023-11-19 09:16:15 +0800

    Add streamlit page

--- langchain_helper.py
+++ langchain_helper.py
@@ -1,28 +1,23 @@
-import streamlit as st
-from langchain.llms import OpenAI
-from langchain.prompts import PromptTemplate
-from langchain.chains import LLMChain
-from langchain.chains import SequentialChain
+from langchain.llms import OpenAI  # 导入Langchain库中的OpenAI模块
+from langchain.prompts import PromptTemplate  # 导入Langchain库中的PromptTemplate模块
+from langchain.chains import LLMChain  # 导入Langchain库中的LLMChain模块
+from dotenv import load_dotenv  # 导入dotenv库，用于加载环境变量
 
-from dotenv import load_dotenv
+load_dotenv()  # 加载.env文件中的环境变量
 
-load_dotenv()
+def generate_pet_name(animal_type, pet_color):
+    llm = OpenAI(temperature=0.7)  # 创建OpenAI模型的实例，设置temperature参数为0.7以调整生成的多样性
 
-
-def generate_pet_name(animal_type, pet_color, openai_api_key):
-    llm = OpenAI(temperature=0.7, openai_api_key=openai_api_key)
-
+    # 创建PromptTemplate实例，用于构造输入提示
     prompt_template_name = PromptTemplate(
-        input_variables = ['animal_type','pet_color'],
-        template = "I have a {animal_type} pet and I want a cool name for it, it is {pet_color} in color. Suggest me five cool names for my pet."
+        input_variables=['animal_type', 'pet_color'],
+        template="I have a {animal_type} pet and I want a cool name for it. Suggest me five cool names for my pet."
     )
+    name_chain = LLMChain(llm=llm, prompt=prompt_template_name, output_key='pet_name')  # 创建LLMChain实例，将OpenAI模型和PromptTemplate传入
+    response = name_chain({'animal_type': animal_type, 'pet_color': pet_color})  # 使用LLMChain生成宠物名字
 
-    name_chain = LLMChain(llm=llm, prompt=prompt_template_name, output_key="pet_name")
+    return response  # 返回生成的名字
 
-    response = name_chain({'animal_type': animal_type, 'pet_color': pet_color})
-
-
-    return response
-
+# 当该脚本作为主程序运行时，执行以下代码
 if __name__ == "__main__":
-    print(generate_pet_name("Dog", "Black"))
+    print(generate_pet_name('cow', 'black'))  # 调用generate_pet_name函数，并打印返回的结果
--- main.py
+++ main.py
@@ -1,23 +1,18 @@
-from langchain.llms import OpenAI  # 导入Langchain库中的OpenAI模块
-from langchain.prompts import PromptTemplate  # 导入Langchain库中的PromptTemplate模块
-from langchain.chains import LLMChain  # 导入Langchain库中的LLMChain模块
-from dotenv import load_dotenv  # 导入dotenv库，用于加载环境变量
+import langchain_helper as lch  # 导入名为langchain_helper的模块，并使用别名lch
+import streamlit as st  # 导入Streamlit库，并使用别名st
 
-load_dotenv()  # 加载.env文件中的环境变量
+st.title("Pets name generator")  # 在Streamlit应用中设置标题
 
-def generate_pet_name(animal_type):
-    llm = OpenAI(temperature=0.7)  # 创建OpenAI模型的实例，设置temperature参数为0.7以调整生成的多样性
+# 通过侧边栏选择宠物类型
+animal_type = st.sidebar.selectbox("Select animal type", ["dog", "cat", "cow", "horse", "pig", "sheep"])
 
-    # 创建PromptTemplate实例，用于构造输入提示
-    prompt_template_name = PromptTemplate(
-        input_variables=['animal_type'],
-        template="I have a {animal_type} pet and I want a cool name for it. Suggest me five cool names for my pet."
-    )
-    name_chain = LLMChain(llm=llm, prompt=prompt_template_name)  # 创建LLMChain实例，将OpenAI模型和PromptTemplate传入
-    response = name_chain({'animal_type': animal_type})  # 使用LLMChain生成宠物名字
+# 根据宠物类型设置宠物颜色，使用侧边栏的文本区域输入
+if animal_type in ['dog', 'cat', 'cow', 'horse', 'pig', 'sheep']:
+    pet_color = st.sidebar.text_area(label=f"What color is your {animal_type}?", max_chars=15)
+else:
+    pet_color = st.sidebar.text_area(label="What color is your pet?", max_chars=15)
 
-    return response  # 返回生成的名字
-
-# 当该脚本作为主程序运行时，执行以下代码
-if __name__ == "__main__":
-    print(generate_pet_name('cat'))  # 调用generate_pet_name函数，并打印返回的结果
+# 如果有输入颜色，调用generate_pet_name函数生成宠物名字并显示
+if pet_color:
+    response = lch.generate_pet_name(animal_type, pet_color)
+    st.text(response['pet_name'])
