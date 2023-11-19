4e2fd026f45bc3cc674df219eed90fdd87d6ee62
zgpeace <zgpeace@gmail.com>
2023-11-20 00:21:04 +0800

    retrieve youtube script

--- langchain_helper.py
+++ langchain_helper.py
@@ -1,23 +1,70 @@
-from langchain.llms import OpenAI  # 导入Langchain库中的OpenAI模块
-from langchain.prompts import PromptTemplate  # 导入Langchain库中的PromptTemplate模块
-from langchain.chains import LLMChain  # 导入Langchain库中的LLMChain模块
-from dotenv import load_dotenv  # 导入dotenv库，用于加载环境变量
+# 从langchain包和其他库中导入必要的模块
+from langchain.document_loaders import YoutubeLoader  # 导入YoutubeLoader，用于从YouTube视频加载数据
+from langchain.text_splitter import RecursiveCharacterTextSplitter  # 导入用于处理长文档的文本分割器
+from langchain.embeddings.openai import OpenAIEmbeddings  # 导入OpenAIEmbeddings，用于生成文档嵌入向量
+from langchain.vectorstores import FAISS  # 导入FAISS，用于大数据集中高效的相似性搜索
+from langchain.llms import OpenAI  # 导入OpenAI，用于访问语言模型功能
+from langchain import PromptTemplate  # 导入PromptTemplate，用于创建结构化的语言模型提示
+from langchain.chains import LLMChain  # 导入LLMChain，用于构建使用语言模型的操作链
+from dotenv import load_dotenv  # 导入load_dotenv，用于从.env文件加载环境变量
 
-load_dotenv()  # 加载.env文件中的环境变量
+load_dotenv()  # 从.env文件加载环境变量
 
-def generate_pet_name(animal_type, pet_color):
-    llm = OpenAI(temperature=0.7)  # 创建OpenAI模型的实例，设置temperature参数为0.7以调整生成的多样性
+embedding = OpenAIEmbeddings()  # 初始化OpenAI嵌入向量的实例，用于生成文档嵌入向量
 
-    # 创建PromptTemplate实例，用于构造输入提示
-    prompt_template_name = PromptTemplate(
-        input_variables=['animal_type', 'pet_color'],
-        template="I have a {animal_type} pet and I want a cool name for it. Suggest me five cool names for my pet."
+# YouTube视频的URL
+video_url = "https://youtu.be/-Osca2Zax4Y?si=iy0iePxzUy_bUayO"
+
+def create_vector_db_from_youtube_url(video_url: str) -> FAISS:
+    # 加载YouTube视频字幕
+    loader = YoutubeLoader.from_youtube_url(video_url)
+    transcript = loader.load()
+
+    # 将字幕分割成较小的片段
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=100)
+    docs = text_splitter.split_documents(transcript)
+
+    # 从文档片段创建FAISS数据库
+    db = FAISS.from_documents(docs, embedding)
+    return db
+
+def get_response_from_query(db, query, k=4):
+    # 对给定查询执行数据库的相似性搜索
+    docs = db.similarity_search(query, k=k)
+
+    # 连接前几个文档的内容
+    docs_page_content = " ".join([d.page_content for d in docs])
+    
+    # 初始化一个OpenAI语言模型
+    llm = OpenAI(model="text-davinci-003")
+
+    # 定义语言模型的提示模板
+    prompt = PromptTemplate(
+        input_variables=["question", "docs"],
+        template = """
+        You are a helpful assistant that that can answer questions about youtube videos 
+        based on the video's transcript.
+        
+        Answer the following question: {question}
+        By searching the following video transcript: {docs}
+        
+        Only use the factual information from the transcript to answer the question.
+        
+        If you feel like you don't have enough information to answer the question, say "I don't know".
+        
+        Your answers should be verbose and detailed.
+    """,
     )
-    name_chain = LLMChain(llm=llm, prompt=prompt_template_name, output_key='pet_name')  # 创建LLMChain实例，将OpenAI模型和PromptTemplate传入
-    response = name_chain({'animal_type': animal_type, 'pet_color': pet_color})  # 使用LLMChain生成宠物名字
 
-    return response  # 返回生成的名字
+    # 使用定义的提示创建一个语言模型链
+    chain = LLMChain(llm=llm, prompt=prompt)
 
-# 当该脚本作为主程序运行时，执行以下代码
-if __name__ == "__main__":
-    print(generate_pet_name('cow', 'black'))  # 调用generate_pet_name函数，并打印返回的结果
+    # 使用查询和连接的文档运行链
+    response = chain.run(question=query, docs=docs_page_content)
+
+    # 通过替换换行符来格式化响应
+    response = response.replace("\n", " ")
+    return response, docs
+
+# 示例用法：从YouTube视频URL创建向量数据库
+# print(create_vector_db_from_youtube_url(video_url))
--- main.py
+++ main.py
@@ -1,18 +1,35 @@
-import langchain_helper as lch  # 导入名为langchain_helper的模块，并使用别名lch
-import streamlit as st  # 导入Streamlit库，并使用别名st
+import streamlit as st  # 导入Streamlit库，用于创建Web应用程序
+import langchain_helper as lch  # 导入自定义模块'langchain_helper'，用于处理langchain操作
+import textwrap  # 导入textwrap模块，用于格式化文本
 
-st.title("Pets name generator")  # 在Streamlit应用中设置标题
+st.title("YouTube Assistant")  # 设置Streamlit网页应用的标题
 
-# 通过侧边栏选择宠物类型
-animal_type = st.sidebar.selectbox("Select animal type", ["dog", "cat", "cow", "horse", "pig", "sheep"])
+# 使用Streamlit的侧边栏功能来创建输入表单
+with st.sidebar:
+    # 在侧边栏中创建一个表单
+    with st.form(key='my_form'):
+        # 创建一个文本区域用于输入YouTube视频URL
+        youtube_url = st.sidebar.text_area(
+            label="What is the YouTube video URL?",
+            max_chars=50
+        )
+        # 创建一个文本区域用于输入关于YouTube视频的查询
+        query = st.sidebar.text_area(
+            label="Ask me about the video?",
+            max_chars=50,
+            key="query"
+        )
+        
+        # 创建一个提交表单的按钮
+        submit_button = st.form_submit_button(label='Submit')
 
-# 根据宠物类型设置宠物颜色，使用侧边栏的文本区域输入
-if animal_type in ['dog', 'cat', 'cow', 'horse', 'pig', 'sheep']:
-    pet_color = st.sidebar.text_area(label=f"What color is your {animal_type}?", max_chars=15)
-else:
-    pet_color = st.sidebar.text_area(label="What color is your pet?", max_chars=15)
-
-# 如果有输入颜色，调用generate_pet_name函数生成宠物名字并显示
-if pet_color:
-    response = lch.generate_pet_name(animal_type, pet_color)
-    st.text(response['pet_name'])
+# 检查是否同时提供了查询和YouTube URL
+if query and youtube_url:
+    # 从YouTube视频URL创建向量数据库
+    db = lch.create_vector_db_from_youtube_url(youtube_url)
+    # 根据向量数据库获取查询的响应
+    response, docs = lch.get_response_from_query(db, query)
+    # 在应用程序中显示一个副标题“回答：”
+    st.subheader("Answer：")
+    # 显示响应，格式化为每行85个字符
+    st.text(textwrap.fill(response, width=85))
