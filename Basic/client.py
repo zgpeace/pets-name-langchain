12d3863d1e821ed3cc78af691fc4bed3a8378453
zgpeace <zgpeace@gmail.com>
2023-12-24 22:10:22 +0800

    update test

--- Basic/client.py
+++ Basic/client.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 
 load_dotenv()  
 
-remote_chain = RemoteRunnable("http://localhost:8000/category_chain/invoke")
+remote_chain = RemoteRunnable("http://localhost:8000/category_chain")
 response = remote_chain.invoke({"text": "colors"})
 print('colors >> ', response)
 # >> ['red', 'blue', 'green', 'yellow', 'orange']
